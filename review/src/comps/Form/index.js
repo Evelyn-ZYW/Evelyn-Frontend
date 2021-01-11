d0e9a399ae0bedae34e80d0fe03f28ed0389fcfd
Evelyn <evelynzdragon@gmail.com>
2021-01-11 14:21:06 -0800

    useState in pages. and rarely in comps. One scenario - if it does not affect other comps

--- review/src/comps/Form/index.js
+++ review/src/comps/Form/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -6,6 +6,7 @@ const Container = styled.div`
     min-height: ${props=>props.height?props.height:"100px"};
     max-width: ${props=>props.width?props.width:"50%"};
     max-height: ${props=>props.height?props.height:"300px"};
+    padding: 10px;
     display: flex;
     flex-direction: column;
 
@@ -28,22 +29,29 @@ const FormCheckbox = styled.input.attrs({ type: 'checkbox' })``;
 //They are to capture the effects after clicked on the button or container
 //The 2 interactions I want to capture is when I click on the container and the button
 const Form = ({name, width, height, bgcolor, onFormComplete, onContainerSelect}) => {
+
+    const [username, setUsername] = useState("");
+    const [pass, setPass] = useState("");
+    const [check1, setCheck1] = useState(false)
+    const [check2, setCheck2] = useState(false)
+    const [check3, setCheck3] = useState(false);
+
     return <Container onClick={()=>{
         onContainerSelect(name)
     }} width={width} height={height} bgcolor={bgcolor}>
-        <FormInput type='text' placeholder='name' />
-        <FormInput type='password' placeholder='pass' />
+        <FormInput type='text' placeholder='name' onChange={(e)=>{setUsername(e.target.value)}}/>
+        <FormInput type='password' placeholder='pass' onChange={(e)=>{setPass(e.target.value)}}/>
         <span>
-            <FormCheckbox /> I agree to sell my soul
+            <FormCheckbox onChange={(e)=>{setCheck1(e.target.checked)}}/> I agree to sell my soul
         </span>
         <span>
-            <FormCheckbox /> I agree to sell my life
+            <FormCheckbox onChange={(e)=>{setCheck2(e.target.checked)}}/> I agree to sell my life
         </span>
         <span>
-            <FormCheckbox /> I agree to give up all of the above
+            <FormCheckbox onChange={(e)=>{setCheck3(e.target.checked)}}/> I agree to give up all of the above
         </span>
         <FormButton onClick={()=>{
-            onFormComplete()
+            onFormComplete(username, pass, check1, check2, check3)
         }}>Sign up</FormButton>
     </Container>
 }
--- review/src/pages/main/index.js
+++ review/src/pages/main/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 // import Form from '../../comps/Form'; | //this will get tiring and confusing, below is another way to import
 import Form from 'comps/Form';
 
 const Main = () => {
 
+    const [clickedForm, setClickedForm] = useState(null);
+
+
     const HandleContainerSelect = (name) => {
-        alert("clicked container "+name);
+        // alert("clicked container " + name)
+        setClickedForm(name);
     }
 
-    const HandleFormComplete = () => {
-
+    const HandleFormComplete = (username, pass, check1, check2, check3) => {
+        console.log(username, pass, check1, check2, check3)
     }
 
     return <div className="main">
@@ -19,11 +24,19 @@ const Main = () => {
                 onContainerSelect={HandleContainerSelect}
                 onFormComplete={HandleFormComplete}
                 name="register"
+                bgcolor={clickedForm === "register" ? "red" : null}
             />
             <Form
                 onContainerSelect={HandleContainerSelect}
                 onFormComplete={HandleFormComplete}
                 name="signup"
+                bgcolor={clickedForm === "signup" ? "red" : null}
+            />
+            <Form
+                onContainerSelect={HandleContainerSelect}
+                onFormComplete={HandleFormComplete}
+                name="beast"
+                bgcolor={clickedForm === "beast" ? "red" : null}
             />
         </div>
         <div className="right"></div>
