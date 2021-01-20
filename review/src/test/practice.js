64832579746a08a2d5fbd6263f8e6959494f5056
Evelyn <evelynzdragon@gmail.com>
2021-01-19 22:09:31 -0800

    some practices

---
+++ review/src/test/practice.js
@@ -0,0 +1,10 @@
+const movies = require("./movies.json");
+
+//take the movies data and try to maneuver it
+
+//slice last 10
+console.log(movies)
+var sliced = movies.slice()
+//splice out a few movies in the middle
+//sort movies by their name/year/id
+//filter movies by keywords, tags, storyline
