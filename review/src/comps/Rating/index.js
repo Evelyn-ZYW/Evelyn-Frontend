a0e192674a61577c67496342e6116112b82c1bf4
= <=>
2021-04-10 11:50:42 -0700

    commit

---
+++ review/src/comps/Rating/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+//createArray is a helper function to create an array of stars that will be displayed on the page; length is number of stars in the array
+const createArray = (length) => [...Array(length)]
+//the Star Component
+const Star = ({ selected = false, onSelect }) => {
+  return (
+    <FaStar
+      color={selected ? "red" : "grey"}
+      onClick={onSelect}
+    />
+  )
+}
+//the Rating Component
+const Rating = ({ totalStars }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return createArray(totalStars).map((o, i) => <Star
+    key={i}
+    selected={selectedStars > i}
+    onSelect={() => setSelectedStars(i + 1)}
+  />)
+}
+
+export default Rating;
---
+++ review/src/pages/practice/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rating from 'comps/Rating';
+
+const Practice = () => {
+  return (
+    <div className="test">
+      <Rating totalStars={5} />
+    </div>
+  )
+}
+export default Practice;
